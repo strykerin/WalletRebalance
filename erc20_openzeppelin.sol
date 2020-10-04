7a40d7ca934f7fdd71e9237f92cb68915e3fd032
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-03 23:15:40 -0300

    remove reference to ERC20Detailed contract

--- erc20_openzeppelin.sol
+++ erc20_openzeppelin.sol
@@ -1,10 +1,9 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract GLDToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("Gold", "GLD", 18) public {
+contract GLDToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20Detailed("Gold", "GLD") public {
         _mint(msg.sender, initialSupply);
     }
 }
