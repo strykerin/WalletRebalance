719d345e73cd71c50bad651397e85de253af7158
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-03 23:24:45 -0300

    delete old erc20 token and change compiler version

--- erc20_openzeppelin.sol
+++ erc20_openzeppelin.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.7.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20Detailed("Gold", "GLD") public {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") public {
         _mint(msg.sender, initialSupply);
     }
 }
