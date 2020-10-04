ece0ce7f97183e3f9ee5018718f28a48f00b76e1
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-04 11:55:33 -0300

    Create erc20 token contract

--- contracts/erc20_openzeppelin.sol
+++ contracts/erc20_openzeppelin.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") public {
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("My Token", "MTN") public {
         _mint(msg.sender, initialSupply);
     }
 }
