83571b7f864ed0b2bbac67f86872c4ad051f6dd9
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-12 15:20:31 -0300

    transfer all DappTokens to TokenFarm smart contract when deployed

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("My Token", "MTN") public {
+contract DappToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Dapp Token", "DAPP") public {
         _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,15 @@
-var MyToken = artifacts.require("MyToken");
+var DappToken = artifacts.require("DappToken");
+var TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-    deployer.deploy(MyToken, 1000000);
-    // Additional contracts can be deployed here
+module.exports = async function(deployer) {
+    // deploy Dapptoken
+    await deployer.deploy(DappToken, 1000000);
+    const dappToken = await DappToken.deployed();
+
+    // deploy tokenFarm
+    await deployer.deploy(TokenFarm, dappToken.address);
+    const tokenFarm = await TokenFarm.deployed();
+
+    // transfer dappTokens to tokenFarm
+    await dappToken.transfer(tokenFarm.address, '1000000')
 };
