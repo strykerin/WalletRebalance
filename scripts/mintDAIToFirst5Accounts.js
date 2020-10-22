26eb4dd0cc3495ce86070a917afa116f41136750
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-22 00:50:56 -0300

    add script to mint 100 DAI to first 5 accounts

--- scripts/mintDAIToFirst5Accounts.js
+++ scripts/mintDAIToFirst5Accounts.js
@@ -38,32 +38,3 @@ module.exports = async function(callback) {
 
   callback();
 }
-
-
-
-
-// contract('Truffle Mint DAI', async accounts => {
-//   it('should send ether to the DAI address', async () => {
-//     // Send 0.1 eth to userAddress to have gas to send an ERC20 tx.
-//     await web3.eth.sendTransaction({
-//       from: accounts[0],
-//       to: userAddress,
-//       value: ether('0.1')
-//     });
-//     const ethBalance = await balance.current(userAddress);
-//     expect(new BN(ethBalance)).to.be.bignumber.least(new BN(ether('0.1')));
-//   });
-
-//   it('should mint DAI for our first 5 generated accounts', async () => {
-//     // Get 100 DAI for first 5 accounts
-//     await asyncForEach(accounts.slice(0, 5), async account => {
-//       // daiAddress is passed to ganache-cli with flag `--unlock`
-//       // so we can use the `transfer` method
-//       await daiContract.methods
-//         .transfer(account, ether('100').toString())
-//         .send({ from: userAddress, gasLimit: 800000 });
-//       const daiBalance = await daiContract.methods.balanceOf(account).call();
-//       expect(new BN(daiBalance)).to.be.bignumber.least(ether('100'));
-//     });
-//   });
-// });
