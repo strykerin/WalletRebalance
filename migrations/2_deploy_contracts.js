382cd3a58631594692812424db16fb4fa85573d7
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-22 00:46:34 -0300

    finish script to deploy vault, controller and strategy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,40 @@
 var DappToken = artifacts.require("DappToken");
 var TokenFarm = artifacts.require("TokenFarm");
+var YVault = artifacts.require("yVault");
+var Controller = artifacts.require("Controller");
+var StrategyDAICompoundBasic = artifacts.require("StrategyDAICompoundBasic");
 
 module.exports = async function(deployer, network, accounts) {
-    // deploy Dapptoken
-    await deployer.deploy(DappToken);
-    const dappToken = await DappToken.deployed();
+    // // deploy Dapptoken
+    // await deployer.deploy(DappToken);
+    // const dappToken = await DappToken.deployed();
 
-    // deploy tokenFarm
-    await deployer.deploy(TokenFarm, dappToken.address);
-    const tokenFarm = await TokenFarm.deployed();
+    // // deploy tokenFarm
+    // await deployer.deploy(TokenFarm, dappToken.address);
+    // const tokenFarm = await TokenFarm.deployed();
 
-    // transfer dappTokens to tokenFarm
-    const totalSupply = await dappToken.totalSupply();
-    // await dappToken.transfer(tokenFarm.address, totalSupply);
-    await dappToken.transfer(accounts[0], totalSupply);
-};
+    // // transfer dappTokens to tokenFarm
+    // const totalSupply = await dappToken.totalSupply();
+    // // await dappToken.transfer(tokenFarm.address, totalSupply);
+    // await dappToken.transfer(accounts[0], totalSupply);
+
+    const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
+    const rewardsAddress = "0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde";
+
+    // *** DEPLOY SMART CONTRACTS ***
+    await deployer.deploy(Controller, rewardsAddress);
+    const controller = await Controller.deployed();
+    // deploy yVault
+    await deployer.deploy(YVault, daiAddress, controller.address);
+    const yVault = await YVault.deployed();
+    // deploy daiCompStrategy
+    await deployer.deploy(StrategyDAICompoundBasic, controller.address);
+    const strategy = await StrategyDAICompoundBasic.deployed();
+
+    // *** SET SMART CONTRACT PROPERTIES ***
+    // set Vault in Controller
+    await controller.setVault(daiAddress, yVault.address);
+    // set Strategy in Controller
+    await controller.approveStrategy(daiAddress, strategy.address);
+    await controller.setStrategy(daiAddress, strategy.address);
+}
